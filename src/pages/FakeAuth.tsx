d47136c76831ea5fcf92efde60e39d5d456069a3
Sebestyen Szabo <szabosebestyen.mail@gmail.com>
2021-09-21 23:07:50 +0200

    Remove unnecessary routes

--- src/pages/FakeAuth.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-function FakeAuth() {
-  return <div></div>;
-}
-
-export default FakeAuth;
--- src/pages/Router.tsx
+++ src/pages/Router.tsx
@@ -1,9 +1,6 @@
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 import NotFound from "./NotFound";
 import Home from "./Home";
-import FakeAuth from "./FakeAuth";
-import Login from "./Login";
-import User from "./User";
 import UserProfile from "./UserProfile";
 import Post from "./Post";
 
@@ -14,24 +11,12 @@ const Router = () => (
         <Home />
       </Route>
 
-      <Route exact path="/login">
-        <Login />
-      </Route>
-
-      <Route exact path="/fake-auth">
-        <FakeAuth />
-      </Route>
-
       <Route exact path="/post/:postId">
         <Post />
       </Route>
 
       <Route exact path="/profile">
         <UserProfile />
-      </Route>
-
-      <Route exact path="/user/:userId">
-        <User />
       </Route>
 
       <Route path="/*">
