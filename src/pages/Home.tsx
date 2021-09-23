cdf35db2ea19d59cdb99a4f4e0bce7af5cf7306d
Sebestyen Szabo <szabosebestyen.mail@gmail.com>
2021-09-23 10:29:35 +0200

    Add navigation back to home

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -4,7 +4,6 @@ import Posts from "../components/Posts";
 function Home() {
   return (
     <div>
-      <h1 className="m-3">News</h1>
       <Posts />
     </div>
   );
--- src/pages/Router.tsx
+++ src/pages/Router.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Switch, Route } from "react-router-dom";
+import { BrowserRouter, Switch, Route, Link } from "react-router-dom";
 import NotFound from "./NotFound";
 import Home from "./Home";
 import UserProfile from "./UserProfile";
@@ -6,6 +6,12 @@ import PostWithComments from "./PostWithComments";
 
 const Router = () => (
   <BrowserRouter>
+    <h1 className="m-3">
+      <Link to="/" className="text-decoration-none">
+        News Feed
+      </Link>
+    </h1>
+
     <Switch>
       <Route exact path="/">
         <Home />
